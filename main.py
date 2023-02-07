e9c80affa32475df263d99deb7dab6387e1cc9ab
Sergio O <babyharpseal4@gmail.com>
2023-02-07 10:59:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from skyfield.api import wgs84, load
 from skyfield import api
 from skyfield.framelib import itrs
 import datetime
-import sense_hat as sense
-import csv
+from sense_hat import SenseHat
 import time
 
 # Define variables
 ts = load.timescale()
+sense = SenseHat()
 eph = load('de421.bsp')
 earth, moon = eph['earth'], eph['moon']
 
@@ -16,7 +16,7 @@ earth, moon = eph['earth'], eph['moon']
 while True:
     now = datetime.datetime.now()
     humidity = sense.get_humidity()
-    pressure = sense.get_pressure()
+    press = sense.get_pressure()
     temperature = sense.get_temperature()
     timenow = time
     
@@ -31,16 +31,10 @@ while True:
 
     # Convert the right ascension value from hours to degrees
     ra = ra._degrees / 15
-    ra_formatted = '{:.4f}'.format(ra)
-    dec_formatted = '{:.4f}'.format(dec.degrees)
-
-    header = ['Pressure', 'Humidity', 'Temperature', 'Declination', 'Right Ascension', 'Timestamp']
-    row = [press, humidity, temperature, '{:.4f}'.format(dec.degrees), '{:.4f}'.format(ra), now.strftime("%Y-%m-%d %H:%M:%S")]
-    
-    with open('data.csv', 'a', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerow(row)
+    writevar = "Pressure: " + str(press) + " | Humidity: " + str(humidity) + " | Temperature: " + str(temperature) + " | Timestamp: " + now.strftime("%Y-%m-%d %H:%M:%S") + "\n"
+    with open('data.txt', "a") as f:
+        f.write(writevar)
 
     # Wait for 30 seconds before redoing everything
     time.sleep(30)
+
